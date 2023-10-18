4add21e0be6506f2636f183a3d2af1a98c0de339
Derek Hassan <dhass421@gmail.com>
2023-10-18 06:46:33 -0400

    Change response to JSON, add status codes

--- handler.go
+++ handler.go
@@ -1,25 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"log"
 	"net/http"
 	"os"
 	"time"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
+func writeResponse(w http.ResponseWriter, message string, statusCode int) {
+	w.WriteHeader(statusCode)
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := make(map[string]string)
+	resp["message"] = message
+
+	jsonResp, err := json.Marshal(resp)
+
+	if err != nil {
+		log.Fatalf("Error in JSON marshal: %s", err)
+	}
+
+	w.Write(jsonResp)
+}
+
+func timezoneHandler(w http.ResponseWriter, r *http.Request) {
 	timezone := r.URL.Query().Get("tz")
 	timeNow := time.Now()
-	if timezone != "" {
-		loc, err := time.LoadLocation(timezone)
-		if err != nil {
-			fmt.Fprintf(w, "%v", err)
-		}
-		fmt.Fprint(w, timeNow.In(loc))
-	} else {
-		fmt.Fprint(w, "No timezone supplied!")
+	if timezone == "" {
+		writeResponse(w, "No timezone supplied!", http.StatusBadRequest)
+		return
 	}
+	loc, err := time.LoadLocation(timezone)
+	if err != nil {
+		log.Fatalf("Error in loading timezone location: %v", err)
+	}
+
+	writeResponse(w, timeNow.In(loc).String(), http.StatusOK)
 }
 
 func main() {
@@ -27,7 +44,7 @@ func main() {
 	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
 		listenAddr = ":" + val
 	}
-	http.HandleFunc("/api/TimeZoneHttpTrigger", helloHandler)
+	http.HandleFunc("/api/TimeZoneHttpTrigger", timezoneHandler)
 	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
